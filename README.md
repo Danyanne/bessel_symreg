099ae40525f5b89122fbd3ec308a2bf828ab8189
Daniel Ctvrtecka <dan.ctvrta@email.cz>
2025-09-04 11:48:40 +0100

    Renaming the imports from BesselML to symreg

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# BesselML
+# bessel_symreg
 Project conducted in Summer 2025 in an internship at Oxford University. It was suprivised by Constantinos Skordis (CEICO) and Deaglan Bartlett (Oxford University)
--- src/symreg/__init__.py
+++ src/symreg/__init__.py
@@ -11,5 +11,4 @@ __version__ = "0.1.0"
 __author__ = "Daniel C."
 __email__ = "dan.ctvrta@email.cz"
 
-# Explicitly declare what should be imported with "from BesselML import *"
 __all__ = ['Problem', 'Solution', 'Promising_solution', 'create_arbitrary_constraint']
